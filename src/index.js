d72e28b27841cd730c5f8e73bebb7ea01660b668
Tejas Gawade <tgawade092@gmail.com>
2024-11-08 12:18:30 +0530

    Add post endpoint to get video information

--- src/index.js
+++ src/index.js
@@ -16,3 +16,34 @@ app.listen(PORT, () => {
 app.get("/", async(req, res) => {
     res.send("Working fine..");
 })
+
+app.post("/api/video-info", async(req, res) => {
+    const { url }  = req.body;
+
+    if(!ytdl.validateURL(url)) {
+        return res
+        .status(400)
+        .json({ error: "Invalid url"});
+    }
+
+    try {
+        const info = await ytdl.getInfo(url);
+        const videoDetails = {
+            title: info.videoDetails.title,
+            thumbnail: info.videoDetails.thumbnail,
+            formats: info.formats
+            .filter(format => format.hasAudio && format.hasVideo) 
+            .map(format => ({
+                quality: format.qualityLabel,
+                type: format.container,
+                url: format.url,
+            }))
+        };
+        res.json(videoDetails);
+    } catch (error) {
+        console.log(error);
+        res
+        .status(500)
+        .json({ error: "Failed to fetch video information"})
+    }
+});
